015ffd0152f7411bcb90e67c3e95b9fa84095e1c
VigneshRav <vignesh212000@gmail.com>
2025-05-26 16:42:16 +0530

    Login User completed and API tested via Postman

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,7 +1,11 @@
 import User from "../Models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-// Register User:
+dotenv.config();
+
+//Register User:
 export const registerUser = async (req, res) => {
   try {
     const { UserName, Email, Password } = req.body;
@@ -11,6 +15,33 @@ export const registerUser = async (req, res) => {
     res
       .status(200)
       .json({ message: "User registered successfully", data: newUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Login User:
+export const loginUser = async (req, res) => {
+  try {
+    const { Email, Password } = req.body;
+    const existingUser = await User.findOne({ Email });
+    if (!existingUser) {
+      res.status(404).json({ message: "User not found" });
+    }
+    const passwordMatch = await bcrypt.compare(Password, existingUser.Password);
+    if (!passwordMatch) {
+      res.status(401).json({ message: "Invalid password" });
+    }
+    const token = jwt.sign(
+      { _id: existingUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    existingUser.Token = token;
+    await existingUser.save();
+    res
+      .status(200)
+      .json({ message: "User loggedIn successfully", token: token });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
